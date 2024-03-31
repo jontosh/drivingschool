3a497562c582f4a3895dc0f059721ca1158bbcd2
Yunus <theyuunus@gmail.com>
2024-03-31 14:20:17 +0500

    #fix #jsx added "Input" and "Select" components

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,6 +1,9 @@
 import classNames from "classnames";
 import { Fragment } from "react";
 import FormStyle from "./form.module.scss";
+import { Input, Select } from "antd";
+const { Option } = Select;
+
 export const CustomCheckBox = ({
   className,
   value,
@@ -32,3 +35,55 @@ export const CustomCheckBox = ({
     </Fragment>
   );
 };
+export const CustomInput = ({
+  className,
+  value,
+  name,
+  placeholder,
+  status,
+  size,
+  onChange,
+  spanText,
+}) => {
+  return (
+    <Fragment>
+      <div className={"flex flex-col"}>
+        <Input
+          className={className}
+          type="text"
+          value={value}
+          name={name}
+          placeholder={placeholder}
+          status={status}
+          size={size}
+          onChange={onChange}
+        />
+        <span className={`${className} text-base text-red-500`}>{spanText}</span>
+      </div>
+    </Fragment>
+  );
+};
+
+export const CustomSelect = ({
+  className,
+  value,
+  onChange,
+  options,
+  placeholder
+
+}) => {
+  return (
+    <Fragment>
+      <Select
+        className={className}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      >
+        {options.map(option => (
+          <Option key={option.value} value={option.value}>{option.label}</Option>
+        ))}
+      </Select>
+    </Fragment>
+  )
+}
