657ae4d98ce44a70d92f6509d7517aab47ce6ed9
KhaetbekIT <hayotbek-web@mail.ru>
2024-04-15 02:52:50 +0500

    #fix #jsx #styles Added a TabItem component to the dashboard items and customized the theme of the Tabs component for a more consistent user experience.

---
+++ frontend/src/pages/dashboard/items/tab-content.jsx
@@ -0,0 +1,206 @@
+import ButtonComponent from "@/components/button/index.jsx";
+import IconComponent from "@/components/icons/index.jsx";
+import { Paragraph } from "@/components/title/index.jsx";
+import ColorsContext from "@/context/colors.jsx";
+import { CommentOutlined } from "@ant-design/icons";
+import { Fragment, useContext } from "react";
+import {
+  AiOutlineCloudUpload,
+  AiOutlineFileUnknown,
+  AiOutlineMessage,
+  AiOutlineProject,
+  AiOutlineShoppingCart,
+  AiOutlineSolution,
+  AiOutlineUnorderedList,
+} from "react-icons/ai";
+
+export const UploadFiles = () => {
+  const { colorsObject } = useContext(ColorsContext);
+  return (
+    <Fragment>
+      <div className={"pt-16 pb-12"}>
+        <div className="flex items-center flex-col gap-5 text-center">
+          <IconComponent
+            className={"text-5xl"}
+            icon={<AiOutlineFileUnknown />}
+          />
+          <Paragraph fontSize={"text-base"} fontWeightStrong={500}>
+            There are 0 new text messages received in the last 24 hours.
+          </Paragraph>
+          <Paragraph fontSize={"text-base"} fontWeightStrong={500}>
+            You have <span className="text-red-600">5</span> documents to be
+            confirmed.
+          </Paragraph>
+          <div>
+            <ButtonComponent
+              defaultHoverBg={colorsObject.info}
+              defaultBg={colorsObject.info}
+              defaultHoverColor={colorsObject.main}
+              defaultColor={colorsObject.main}
+              fontSize={14}
+              controlHeight={32}
+              paddingBlock={4}
+              paddingInline={15}
+              borderRadius={3}
+              href={"#!"}
+            >
+              Show all files
+            </ButtonComponent>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+export const Task = () => {
+  const { colorsObject } = useContext(ColorsContext);
+  return (
+    <Fragment>
+      <div className={"pt-16 pb-12"}>
+        <div className="flex items-center flex-col gap-5 text-center">
+          <IconComponent
+            className={"text-5xl"}
+            icon={<AiOutlineUnorderedList />}
+          />
+          <Paragraph fontSize={"text-base"} fontWeightStrong={500}>
+            There are 0 new text messages received in the last 24 hours.
+          </Paragraph>
+          <Paragraph fontSize={"text-base"} fontWeightStrong={500}>
+            You have <span className="text-red-600">5</span> documents to be
+            confirmed.
+          </Paragraph>
+          <div>
+            <ButtonComponent
+              defaultHoverBg={colorsObject.info}
+              defaultBg={colorsObject.info}
+              defaultHoverColor={colorsObject.main}
+              defaultColor={colorsObject.main}
+              fontSize={14}
+              controlHeight={32}
+              paddingBlock={4}
+              paddingInline={15}
+              borderRadius={3}
+              href={"#!"}
+            >
+              Show all tasks
+            </ButtonComponent>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+export const Messages = () => {
+  const { colorsObject } = useContext(ColorsContext);
+  return (
+    <Fragment>
+      <div className={"pt-16 pb-12"}>
+        <div className="flex items-center flex-col gap-5 text-center">
+          <IconComponent className={"text-5xl"} icon={<CommentOutlined />} />
+          <Paragraph fontSize={"text-base"} fontWeightStrong={500}>
+            There are 0 new text messages received in the last 24 hours.
+          </Paragraph>
+          <Paragraph fontSize={"text-base"} fontWeightStrong={500}>
+            You have <span className="text-red-600">5</span> documents to be
+            confirmed.
+          </Paragraph>
+          <div>
+            <ButtonComponent
+              defaultHoverBg={colorsObject.info}
+              defaultBg={colorsObject.info}
+              defaultHoverColor={colorsObject.main}
+              defaultColor={colorsObject.main}
+              fontSize={14}
+              controlHeight={32}
+              paddingBlock={4}
+              paddingInline={15}
+              borderRadius={3}
+              href={"#!"}
+            >
+              Show unread texts
+            </ButtonComponent>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+export const Enrollments = () => {
+  const { colorsObject } = useContext(ColorsContext);
+  return (
+    <Fragment>
+      <div className={"pt-16 pb-12"}>
+        <div className="flex items-center flex-col gap-5 text-center">
+          <IconComponent
+            className={"text-5xl"}
+            icon={<AiOutlineShoppingCart />}
+          />
+          <Paragraph fontSize={"text-base"} fontWeightStrong={500}>
+            There are 0 new text messages received in the last 24 hours.
+          </Paragraph>
+          <Paragraph fontSize={"text-base"} fontWeightStrong={500}>
+            You have <span className="text-red-600">5</span> documents to be
+            confirmed.
+          </Paragraph>
+          <div>
+            <ButtonComponent
+              defaultHoverBg={colorsObject.info}
+              defaultBg={colorsObject.info}
+              defaultHoverColor={colorsObject.main}
+              defaultColor={colorsObject.main}
+              fontSize={14}
+              controlHeight={32}
+              paddingBlock={4}
+              paddingInline={15}
+              borderRadius={3}
+              href={"#!"}
+            >
+              Show website enrollments
+            </ButtonComponent>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+const TabItem = () => {
+  return [
+    {
+      key: "1",
+      label: <span>Upload files 0</span>,
+      children: <UploadFiles />,
+      icon: <AiOutlineCloudUpload />,
+    },
+    {
+      key: "2",
+      label: (
+        <span>
+          Task <span className="text-red-600">18</span>
+        </span>
+      ),
+      children: <Task />,
+      icon: <AiOutlineProject />,
+    },
+    {
+      key: "3",
+      label: (
+        <span>
+          Text message <span className="text-red-600">5</span>
+        </span>
+      ),
+      children: <Messages />,
+      icon: <AiOutlineMessage />,
+    },
+    {
+      key: "4",
+      label: <span>Website enrollments 0</span>,
+      children: <Enrollments />,
+      icon: <AiOutlineSolution />,
+    },
+  ].map((item) => {
+    return { ...item };
+  });
+};
+
+export default TabItem;
