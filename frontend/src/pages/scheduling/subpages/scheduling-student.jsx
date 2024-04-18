3cfcc6f8dee0a2f70ec83592a4a6b195df997c21
KhaetbekIT <hayotbek-web@mail.ru>
2024-04-18 08:24:45 +0500

    #fix #jsx Update student scheduling component with new imports, UI elements, and functionality for improved user experience.

--- frontend/src/pages/scheduling/subpages/scheduling-student.jsx
+++ frontend/src/pages/scheduling/subpages/scheduling-student.jsx
@@ -1,11 +1,16 @@
 import ButtonComponent from "@/components/button/index.jsx";
 import { CustomInput } from "@/components/form/index.jsx";
+import IconComponent from "@/components/icons/index.jsx";
+import Title, { Paragraph } from "@/components/title/index.jsx";
 import ColorsContext from "@/context/colors.jsx";
+import { Calendar, Table } from "antd";
 import { Formik } from "formik";
 import { Fragment, useContext } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
+import { PiCalendarThin } from "react-icons/pi";
+import { NavLink } from "react-router-dom";
 
-const FormSearch = () => {
+const FormResult = () => {
   const { colorsObject } = useContext(ColorsContext);
   return (
     <Fragment>
@@ -79,16 +84,149 @@ const FormSearch = () => {
           );
         }}
       </Formik>
+      <div className="bg-white px-8 rounded-xl">
+        <div className="space-x-7 -mx-8 px-8 border-b border-b-gray-400">
+          <NavLink
+            to={""}
+            className={"hover:text-indigo-500 text-gray-500 text-xl py-8"}
+          >
+            Book my lessons
+          </NavLink>
+          <NavLink
+            to={""}
+            className={"hover:text-indigo-500 text-gray-500 text-xl py-8"}
+          >
+            My schedule
+          </NavLink>
+        </div>
+
+        <div className="py-6 space-y-7">
+          <div className="space-x-4">
+            <NavLink
+              to={""}
+              className={
+                "hover:text-indigo-500 text-gray-500 text-xl py-8 font-medium"
+              }
+            >
+              Book my lesson
+            </NavLink>
+            <NavLink
+              to={""}
+              className={
+                "hover:text-indigo-500 text-gray-500 text-xl py-8 font-medium"
+              }
+            >
+              Aminov Makhsudjon
+            </NavLink>
+          </div>
+
+          <ul className={"space-y-2.5"}>
+            <li className={"cursor-pointer"}>
+              <div className="bg-white shadow-xl py-3 px-8 hover:text-indigo-500 flex justify-between">
+                <Paragraph
+                  fontSize={"text-base text-inherit"}
+                  className={"text-inherit"}
+                >
+                  Click here to view scheduled lessons
+                </Paragraph>
+                <span>+</span>
+              </div>
+            </li>
+            <li className={"cursor-pointer"}>
+              <div className="bg-white shadow-xl py-3 px-8 hover:text-indigo-500 flex justify-between">
+                <Paragraph
+                  fontSize={"text-base text-inherit"}
+                  className={"text-inherit"}
+                >
+                  Click here to view scheduled lessons
+                </Paragraph>
+                <span>+</span>
+              </div>
+            </li>
+          </ul>
+
+          <div className={"grid grid-cols-2 gap-5"}>
+            <div className={"grid grid-cols-2 gap-5"}>
+              <Calendar fullscreen={false} className={"shadow-xl"} />
+              <Calendar fullscreen={false} className={"shadow-xl"} />
+            </div>
+
+            <div className={"flex justify-center flex-col gap-5"}>
+              <ButtonComponent
+                defaultBg={colorsObject.info}
+                defaultHoverBg={colorsObject.info}
+                defaultColor={colorsObject.main}
+                defaultHoverColor={colorsObject.main}
+                className={"flex-shrink-0 flex-grow-0"}
+                borderRadius={5}
+                controlHeight={40}
+              >
+                Refine search
+              </ButtonComponent>
+              <ButtonComponent
+                defaultBg={colorsObject.secondary}
+                defaultHoverBg={colorsObject.secondary}
+                defaultColor={colorsObject.main}
+                defaultHoverColor={colorsObject.main}
+                className={"flex-shrink-0 flex-grow-0"}
+                borderRadius={5}
+                controlHeight={40}
+              >
+                Clear search
+              </ButtonComponent>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={"bg-white p-8 rounded-xl"}>
+        <div className="flex justify-between">
+          <Title level={2} fontSize={"text-indigo-700 flex items-center gap-2"}>
+            <IconComponent
+              icon={<PiCalendarThin />}
+              className={"cursor-text"}
+            />
+            <span>AVAILABLE OPEN SLOTS</span>
+          </Title>
+
+          <div className="space-x-5">
+            <ButtonComponent
+              defaultBg={colorsObject.info}
+              defaultHoverBg={colorsObject.info}
+              defaultColor={colorsObject.main}
+              defaultHoverColor={colorsObject.main}
+              className={"flex-shrink-0 flex-grow-0"}
+              borderRadius={5}
+              controlHeight={40}
+              paddingInline={50}
+            >
+              Refine search
+            </ButtonComponent>
+            <ButtonComponent
+              defaultBg={colorsObject.secondary}
+              defaultHoverBg={colorsObject.secondary}
+              defaultColor={colorsObject.main}
+              defaultHoverColor={colorsObject.main}
+              className={"flex-shrink-0 flex-grow-0"}
+              borderRadius={5}
+              controlHeight={40}
+              paddingInline={53}
+            >
+              Clear search
+            </ButtonComponent>
+          </div>
+        </div>
+
+        <Table className={"pt-5"} />
+      </div>
     </Fragment>
   );
 };
 
 export const SchedulingStudent = () => {
-  const { colorsObject } = useContext(ColorsContext);
-
   return (
-    <Fragment>
-      <FormSearch />
-    </Fragment>
+    <div className={"space-y-5"}>
+      <FormResult />
+    </div>
   );
 };
