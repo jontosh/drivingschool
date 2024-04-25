d27af93e117c77e8ffc9b06851cec49a5bd277c1
ali.yuldashev <aliyuldashev880@gmail.com>
2024-04-25 11:23:16 +0900

    backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+#     APPS
+    "location",
+    "Users",
+
 #     INSTALLED LIBRARIES
     "colorfield",
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
