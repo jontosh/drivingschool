da97866874cfc7ccc03ebc831ec8d441479e67f1
KhaetbekIT <hayotbek-web@mail.ru>
2024-05-13 13:17:14 +0500

    #fix #jsx #styles Remove unused files and update import statements in the index and report-form files. Refactor form styling and class names in ReportForm component, update file paths and workspace settings.

--- frontend/src/pages/report/items/report-form.jsx
+++ frontend/src/pages/report/items/report-form.jsx
@@ -5,10 +5,11 @@ import classNames from "classnames";
 import { Formik } from "formik";
 import { useContext } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
+import ReportFormStyle from "./../report.module.scss";
 
 const ReportForm = ({ className, children, ...props }) => {
-  className = classNames(className, "relative mb-5");
   const { colorsObject } = useContext(ColorsContext);
+
   return (
     <Formik
       initialValues={{ search: "" }}
@@ -25,33 +26,35 @@ const ReportForm = ({ className, children, ...props }) => {
       }}
     >
       {({ handleChange, values, handleSubmit, errors }) => (
-        <form className={className} {...props} onSubmit={handleSubmit}>
-          <div>
-            <label className={"relative w-full shadow-xl"}>
-              <CustomInput
-                colorBorder={colorsObject.primary}
-                placeholder={"Find student"}
-                className={`w-full pl-12 pr-4 py-2.5 text-sm `}
-                classNames={"w-full"}
-                name={"search"}
-                value={values.search}
-                onChange={handleChange}
-              />
-              <span
-                className={"absolute left-4 top-1/2 w-5 h-5 -translate-y-1/2 "}
-              >
-                <AiOutlineSearch />
-              </span>
-            </label>
+        <form
+          className={classNames(className, "relative mb-5 w-80")}
+          {...props}
+          onSubmit={handleSubmit}
+        >
+          <label className={"relative w-full shadow-xl"}>
+            <CustomInput
+              colorBorder={colorsObject.primary}
+              placeholder={"Find student"}
+              className={`w-full pl-12 pr-4 py-2.5 text-sm `}
+              classNames={"w-full"}
+              name={"search"}
+              value={values.search}
+              onChange={handleChange}
+            />
+            <span
+              className={"absolute left-4 top-1/2 w-5 h-5 -translate-y-1/2 "}
+            >
+              <AiOutlineSearch />
+            </span>
+          </label>
 
-            {errors.search && (
-              <FormError
-                className={"bg-white w-full py-2.5 px-1 mt-0 relative z-10"}
-              >
-                {errors.search}
-              </FormError>
-            )}
-          </div>
+          {errors.search && (
+            <FormError
+              className={`w-full py-2.5 px-1 relative ${ReportFormStyle["Form__error"]}`}
+            >
+              {errors.search}
+            </FormError>
+          )}
         </form>
       )}
     </Formik>
