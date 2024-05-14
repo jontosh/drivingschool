b528f3c1918118195ba0f23f99bb6fc220cde778
KhaetbekIT <hayotbek-web@mail.ru>
2024-05-14 12:24:46 +0500

    #fix #jsx Refactor component to include date pickers and form handling for improved user interaction.

--- frontend/src/pages/report/subpages/attendance-sheet-report.jsx
+++ frontend/src/pages/report/subpages/attendance-sheet-report.jsx
@@ -1,7 +1,15 @@
+import ButtonComponent from "@/components/button/index.jsx";
+import { CustomCheckBox, CustomInput } from "@/components/form/index.jsx";
 import { Paragraph } from "@/components/title/index.jsx";
-import { Fragment } from "react";
+import ColorsContext from "@/context/colors.jsx";
+import { DatePicker } from "antd";
+import { Formik } from "formik";
+import { Fragment, useContext, useState } from "react";
+import { FiHelpCircle } from "react-icons/fi";
 
 export const AttendanceSheetReport = ({ ...props }) => {
+  const { colorsObject } = useContext(ColorsContext);
+
   return (
     <Fragment>
       <Paragraph
@@ -10,6 +18,78 @@ export const AttendanceSheetReport = ({ ...props }) => {
       >
         CR Attendance Sheet Report
       </Paragraph>
+
+      <Formik
+        initialValues={{
+          class_number: "",
+          score: false,
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ handleSubmit, handleChange, handleReset, values }) => (
+          <form
+            onSubmit={handleSubmit}
+            className={"bg-white rounded-lg px-10 py-7 space-y-7"}
+          >
+            <div className={"grid grid-cols-2 gap-20"}>
+              <div className={"flex items-end gap-3"}>
+                <CustomInput
+                  colorBorder={"#667085"}
+                  spanText={"Class Number Search"}
+                  placeholder={"Class Number Search"}
+                  className={"flex-grow"}
+                  classNames={"inline-flex w-full flex-col-reverse gap-1.5"}
+                  name="class_number"
+                  value={values.class_number}
+                  onChange={handleChange}
+                />
+                <span className={"pb-2"}>
+                  <FiHelpCircle
+                    className={"text-xl text-[#98A2B3] cursor-pointer"}
+                  />
+                </span>
+              </div>
+
+              <CustomCheckBox
+                className={"justify-end"}
+                name={"score"}
+                onChange={handleChange}
+              >
+                Show Score
+              </CustomCheckBox>
+            </div>
+
+            <div className="text-center space-x-5">
+              <ButtonComponent
+                paddingInline={43}
+                controlHeight={40}
+                borderRadius={5}
+                defaultHoverBg={colorsObject.successHover}
+                defaultBg={colorsObject.success}
+                type={"submit"}
+              >
+                Download
+              </ButtonComponent>
+              <ButtonComponent
+                paddingInline={43}
+                controlHeight={40}
+                borderRadius={5}
+                defaultHoverBg={colorsObject.secondary}
+                defaultBg={colorsObject.secondary}
+                defaultColor={colorsObject.main}
+                defaultHoverColor={colorsObject.main}
+                onClick={() => {
+                  handleReset();
+                }}
+              >
+                CLEAR
+              </ButtonComponent>
+            </div>
+          </form>
+        )}
+      </Formik>
     </Fragment>
   );
 };
