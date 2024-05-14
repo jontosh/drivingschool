defc8392fdba45c9b9dd39b60aa6db769712caa9
KhaetbekIT <hayotbek-web@mail.ru>
2024-05-14 13:02:36 +0500

    #fix #jsx Refactor component to include date pickers and form handling for improved user interaction.

--- frontend/src/pages/report/subpages/attendance-signatures.jsx
+++ frontend/src/pages/report/subpages/attendance-signatures.jsx
@@ -1,7 +1,17 @@
+import ButtonComponent from "@/components/button/index.jsx";
+import { CustomInput, CustomSelect } from "@/components/form/index.jsx";
 import { Paragraph } from "@/components/title/index.jsx";
-import { Fragment } from "react";
+import ColorsContext from "@/context/colors.jsx";
+import { Formik } from "formik";
+import { Fragment, useContext, useState } from "react";
 
 export const AttendanceSignatures = ({ ...props }) => {
+  const { colorsObject } = useContext(ColorsContext);
+  const [Session, setSession] = useState("");
+
+  // func
+  const handleSession = (value) => setSession(value);
+
   return (
     <Fragment>
       <Paragraph
@@ -10,6 +20,82 @@ export const AttendanceSignatures = ({ ...props }) => {
       >
         Attendance Signatures/Scores
       </Paragraph>
+
+      <Formik
+        initialValues={{
+          cr_list: "",
+        }}
+        onSubmit={(values) => {
+          console.log({ ...values, session: Session });
+        }}
+      >
+        {({ handleSubmit, handleReset, values, handleChange }) => (
+          <form
+            onSubmit={handleSubmit}
+            className={"bg-white rounded-lg px-10 py-7 space-y-7"}
+          >
+            <div className="space-y-6 max-w-[490px] mx-auto">
+              <CustomInput
+                colorBorder={"#667085"}
+                spanText={"CR LIST"}
+                placeholder={"SELECT CLASS ROOM"}
+                className={"h-[50px]"}
+                classNames={"inline-flex w-full flex-col-reverse gap-1.5"}
+                name="cr_list"
+                value={values.cr_list}
+                onChange={handleChange}
+              />
+
+              <label className={"space-y-1.5 w-full"}>
+                <span className={"text-gray-500 w-full"}>Session#:</span>
+                <CustomSelect
+                  style={{ width: "100%" }}
+                  placeholder={"SELECT SESSION #"}
+                  options={[
+                    {
+                      value: 1,
+                      label: 1,
+                    },
+                    {
+                      value: 2,
+                      label: 2,
+                    },
+                  ]}
+                  onChange={handleSession}
+                  value={Session ? Session : undefined}
+                />
+              </label>
+            </div>
+
+            <div className="text-center space-x-5">
+              <ButtonComponent
+                paddingInline={43}
+                controlHeight={40}
+                borderRadius={5}
+                defaultHoverBg={colorsObject.successHover}
+                defaultBg={colorsObject.success}
+                type={"submit"}
+              >
+                Attendance Signatures/Scores
+              </ButtonComponent>
+              <ButtonComponent
+                paddingInline={43}
+                controlHeight={40}
+                borderRadius={5}
+                defaultHoverBg={colorsObject.secondary}
+                defaultBg={colorsObject.secondary}
+                defaultColor={colorsObject.main}
+                defaultHoverColor={colorsObject.main}
+                onClick={() => {
+                  handleReset();
+                }}
+              >
+                CLEAR
+              </ButtonComponent>
+            </div>
+          </form>
+        )}
+      </Formik>
     </Fragment>
   );
 };
