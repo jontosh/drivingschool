6580c3a1b3ad3eafd9bd54924ef4b5e548ec73d2
ali yuldashev <aliyuldashev880@gmail.com>
2024-06-30 19:37:03 +0900

    BACKEND: Created password reset endpoints

--- backend/Users/serializer.py
+++ backend/Users/serializer.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
 from .models import  Instructor,Student,Enrollment,FileCategory,UserType,Files,Bill,EmergencyContact,StudentNote,DrivingNote
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.core.mail import send_mail
+from django.urls import reverse
+from mainadmin.models import CustomUser
+from django.contrib.auth.password_validation import validate_password
 
 class InstructorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -50,3 +57,44 @@ class EnrollmentSerializer(serializers.ModelSerializer):
 class AuthTokenSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
+
+
+class PasswordResetSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def validate_email(self, value):
+        try:
+            user = CustomUser.objects.get(email=value)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("User with this email does not exist.")
+        return value
+
+    def save(self):
+        request = self.context.get('request')
+        user = CustomUser.objects.get(email=self.validated_data['email'])
+        token = default_token_generator.make_token(user)
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+
+        self.send_email(user, uid, token)
+
+    def send_email(self, user, uid, token):
+        reset_link = f"{self.context[ 'request' ].scheme}://{self.context[ 'request' ].get_host( )}{reverse('password_reset_confirm', kwargs={ 'uidb64': uid, 'token': token })}"
+        subject = "Password Reset Requested"
+        message = f"Hi {user.username},\n\nYou have requested a password reset. Please click the link below to reset your password:\n{reset_link}\n\nIf you did not request this, please ignore this email.\n\nThank you!"
+        send_mail(
+            subject,
+            message,
+            'aliyuldashev880@gmail.com',
+            [ 'yuldashevjavohir15@gmail.com' ],
+            fail_silently=False,
+        )
+
+class PasswordResetConfirmSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True, validators=[validate_password])
+    confirm_password = serializers.CharField(required=True)
+
+    def validate(self, data):
+        if data['new_password'] != data['confirm_password']:
+            raise serializers.ValidationError("New passwords do not match.")
+        return data
--- backend/Users/views.py
+++ backend/Users/views.py
@@ -1,16 +1,15 @@
-from rest_framework import viewsets, status,permissions
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_decode
+from rest_framework import viewsets, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from scheduling.models import Appointment,TimeSlot
+from mainadmin.models import CustomUser
 from django.shortcuts import render
-from django import forms
-
-from django.contrib.auth import authenticate
 from .models import Instructor, Student, Enrollment, FileCategory, UserType, Files, Bill,EmergencyContact,StudentNote,DrivingNote
-from .serializer import InstructorSerializer, StudentSerializer,  \
-       EnrollmentSerializer, FileCategorySerializer,  UserTypeSerializer, FilesSerializer, BillSerializer,\
-       AuthTokenSerializer,EmergencyContactSerializer,DrivingNoteSerializer,StudentNoteSerializer
-from django.contrib.auth.hashers import check_password
+from .serializer import InstructorSerializer, StudentSerializer, \
+    EnrollmentSerializer, FileCategorySerializer, UserTypeSerializer, FilesSerializer, BillSerializer, \
+    AuthTokenSerializer, EmergencyContactSerializer, DrivingNoteSerializer, StudentNoteSerializer, \
+    PasswordResetSerializer, PasswordResetConfirmSerializer
 # Create your views here.
 
 class EmergencyContactViewSet(viewsets.ModelViewSet):
@@ -103,3 +102,51 @@ class BillViewSet(viewsets.ModelViewSet):
 #                 return Response({'token': token.key})
 #             return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
 #         return Response({'error': 'Invalid data'}, status=status.HTTP_400_BAD_REQUEST)
+
+class PasswordResetView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = PasswordResetSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Password reset email sent."}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PasswordResetConfirmView(APIView):
+    def get(self, request, uidb64, token, *args, **kwargs):
+        try:
+            uid = urlsafe_base64_decode(uidb64).decode( )
+            user = CustomUser.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, CustomUser.DoesNotExist):
+            return Response({ "message": "Invalid token or user ID." }, status=status.HTTP_400_BAD_REQUEST)
+
+        if default_token_generator.check_token(user, token):
+            context = {
+                'uid':   uidb64,
+                'token': token,
+                'email':f"{user.email}"
+            }
+            return render(request, 'change_password.html', context)
+        else:
+            return Response({ "message": "Invalid token or user ID." }, status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request, uidb64, token, *args, **kwargs):
+        try:
+            uid = urlsafe_base64_decode(uidb64).decode( )
+            user = CustomUser.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, CustomUser.DoesNotExist):
+            return Response({ "message": "Invalid token or user ID." }, status=status.HTTP_400_BAD_REQUEST)
+
+        if default_token_generator.check_token(user, token):
+            serializer = PasswordResetConfirmSerializer(data=request.data)
+            if serializer.is_valid( ):
+                if not user.check_password(serializer.validated_data[ 'old_password' ]):
+                    return Response({ "message": "Old password is incorrect." }, status=status.HTTP_400_BAD_REQUEST)
+
+                user.set_password(serializer.validated_data[ 'new_password' ])
+                user.save( )
+                return Response({ "message": "Password has been reset." }, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({ "message": "Invalid token or user ID." }, status=status.HTTP_400_BAD_REQUEST)
