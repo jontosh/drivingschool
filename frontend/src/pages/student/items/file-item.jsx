422b0d41795ee93475080afc0c868be9179d01fc
KhaetbekIT <hayotbek-web@mail.ru>
2024-07-04 16:49:40 +0500

    #fix #jsx Refactored Files and content

---
+++ frontend/src/pages/student/items/file-item.jsx
@@ -0,0 +1,99 @@
+import IconComponent from "@/components/icons/index.jsx";
+import { Paragraph } from "@/components/title/index.jsx";
+import ColorsContext from "@/context/colors.jsx";
+import FilesStyle from "@/pages/student/student-account.module.scss";
+import {
+  useRequestDeleteMutation,
+  useRequestIdQuery,
+} from "@/redux/query/index.jsx";
+import { Tooltip } from "antd";
+import classNames from "classnames";
+import { useContext } from "react";
+import { AiOutlineEye, AiOutlineInfoCircle } from "react-icons/ai";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { SlCloudDownload } from "react-icons/sl";
+import moment from "moment";
+import { Link } from "react-router-dom";
+
+export const FileItem = ({ note, name, file, date, category, by, id }) => {
+  const { colorsObject } = useContext(ColorsContext);
+  const [requestDelete] = useRequestDeleteMutation();
+  const { data: Staffdata } = useRequestIdQuery({
+    path: "/student_account/instructor",
+    id: by ?? 0,
+  });
+
+  const onDelete = async () => {
+    try {
+      const response = await requestDelete({
+        path: "/student_account/files",
+        id,
+      });
+
+      console.log("response", response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="flex gap-3 items-center">
+      <div
+        className={classNames(
+          FilesStyle["File__item"],
+          "py-3.5 flex justify-between items-center gap-40 px-5 bg-white border border-indigo-700 rounded-2xl",
+        )}
+      >
+        <Paragraph className={"text-base"}>{name ?? "File name"}</Paragraph>
+        <Paragraph className={"text-base"}>
+          {category ?? "File format"}
+        </Paragraph>
+      </div>
+
+      <div className={"space-x-1 items-center"}>
+        <Link to={file} target={"_blank"}>
+          <IconComponent
+            className={`text-base border border-indigo-700 rounded-lg ${FilesStyle["File__item"]}`}
+            icon={<AiOutlineEye />}
+            style={{ padding: "4px 4px 0" }}
+          />
+        </Link>
+
+        <Link to={file} download target={"_blank"}>
+          <IconComponent
+            className={`text-base border border-indigo-700 rounded-lg ${FilesStyle["File__item"]}`}
+            icon={<SlCloudDownload />}
+            style={{ padding: "4px 4px 0" }}
+          />
+        </Link>
+
+        <Tooltip
+          title={
+            <div className={"px-5 py-3 space-y-2.5"}>
+              <Paragraph>
+                Uploaded By Staff: {Staffdata?.first_name}{" "}
+                {Staffdata?.last_name}
+              </Paragraph>
+              <Paragraph>
+                Uploaded Date: {moment(date)?.format("YYYY-MM-DD")}
+              </Paragraph>
+            </div>
+          }
+          color={colorsObject.main}
+        >
+          <IconComponent
+            className={`text-base border border-indigo-700 rounded-lg ${FilesStyle["File__item"]}`}
+            icon={<AiOutlineInfoCircle />}
+            style={{ padding: "4px 4px 0" }}
+          />
+        </Tooltip>
+        <IconComponent
+          className={`text-base border border-indigo-700 rounded-lg ${FilesStyle["File__item"]}`}
+          icon={<RiDeleteBin6Line />}
+          style={{ padding: "4px 4px 0" }}
+          onClick={onDelete}
+        />
+      </div>
+    </div>
+  );
+};
--- frontend/src/pages/student/upload.jsx
+++ frontend/src/pages/student/upload.jsx
@@ -1,54 +1,112 @@
+import ButtonComponent from "@/components/button/index.jsx";
 import IconComponent from "@/components/icons/index.jsx";
-import { message, Upload as UPLOAD } from "antd";
-import classNames from "classnames";
+import ColorsContext from "@/context/colors.jsx";
+import { useRequestPostMutation } from "@/redux/query/index.jsx";
+import { Form, Upload as UPLOAD } from "antd";
+import { useContext, useState } from "react";
 import { AiOutlineCloudUpload } from "react-icons/ai";
-const { Dragger } = UPLOAD;
-const props = {
-  name: "file",
-  multiple: true,
-  action: "http://localhost:5173/api/upload",
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== "uploading") {
-      console.log(info.file, info.fileList);
+
+const Upload = ({ id }) => {
+  const [form] = Form.useForm();
+  const [requestPost] = useRequestPostMutation();
+  const [Save, setSave] = useState(false);
+
+  const { colorsObject } = useContext(ColorsContext);
+
+  const normFile = (e) => {
+    console.log("Upload event:", e);
+    e?.fileList?.length === 0 ? setSave(false) : setSave(true);
+
+    if (Array.isArray(e)) {
+      return e;
     }
-    if (status === "done") {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === "error") {
-      message.error(`${info.file.name} file upload failed.`);
+    return e?.fileList;
+  };
+
+  const props = {
+    name: "file",
+    action: import.meta.env.VITE_API_URL + "/media/files/student/",
+    className: "w-full",
+    listType: "picture",
+    beforeUpload(file) {
+      return new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+          const img = document.createElement("img");
+          img.src = reader.result;
+          img.onload = () => {
+            const canvas = document.createElement("canvas");
+            canvas.width = img.naturalWidth;
+            canvas.height = img.naturalHeight;
+            const ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0);
+            ctx.fillStyle = "red";
+            ctx.textBaseline = "middle";
+            ctx.font = "33px Arial";
+            ctx.fillText("Ant Design", 20, 20);
+            canvas.toBlob((result) => resolve(result));
+          };
+        };
+      });
+    },
+  };
+
+  const onFinish = async (values) => {
+    try {
+      // console.log("Received values of form: ", values);
+      // console.log("Received values of file: ", values["file"][0]);
+      const response = await requestPost({
+        path: "/student_account/files/",
+        data: {
+          ...values,
+          name: values["file"][0]?.name,
+          file: values["file"][0],
+          date: values["file"][0]?.lastModifiedDate,
+          student: id,
+          by: "094b1a3c-88b4-464d-8ea6-2ef273ffcde3", // default value @todo
+        },
+      });
+
+      // console.log("Received values of response: ", response);
+    } catch (error) {
+      console.error(error);
     }
-  },
-  onDrop(e) {
-    console.log("Dropped files", e.dataTransfer.files);
-  },
-  defaultFileList: [
-    {
-      uid: "1",
-      name: "xxx.png",
-      status: "done",
-      url: "http://www.baidu.com/xxx.png",
-      percent: 33,
-    },
-    {
-      uid: "2",
-      name: "zzz.png",
-      status: "error",
-      response: "Server Error 500",
-      // custom error message to show
-      url: "http://www.baidu.com/zzz.png",
-    },
-  ],
-};
-const Upload = ({ children, className }) => {
-  className = classNames(className);
+  };
+
   return (
-    <Dragger {...props} className={className} style={{ background: "#fff" }}>
-      <IconComponent
-        className={"text-indigo-700 text-6xl mb-2.5"}
-        icon={<AiOutlineCloudUpload />}
-      />
-      {children}
-    </Dragger>
+    <Form form={form} onFinish={onFinish} layout={"vertical"}>
+      <Form.Item className={"w-full"}>
+        <Form.Item name={"file"}>
+          <UPLOAD.Dragger {...props}>
+            <IconComponent
+              className={"text-indigo-700 text-6xl mb-2.5"}
+              icon={<AiOutlineCloudUpload />}
+            />
+            <p className="ant-upload-text">
+              Click or drag file to this area to upload
+            </p>
+            <p className="ant-upload-hint">
+              Support for a single or bulk upload.
+            </p>
+          </UPLOAD.Dragger>
+        </Form.Item>
+      </Form.Item>
+      {Save && (
+        <div className="text-center">
+          <ButtonComponent
+            defaultBg={colorsObject.info}
+            defaultHoverBg={colorsObject.infoHover}
+            controlHeight={40}
+            paddingInline={43}
+            borderRadius={5}
+            type={"submit"}
+          >
+            Save
+          </ButtonComponent>
+        </div>
+      )}
+    </Form>
   );
 };
 export default Upload;
