44fdf9e1757d03964427c11d75612147fa2439f6
KhaetbekIT <hayotbek-web@mail.ru>
2024-07-12 11:20:46 +0500

    #fix #js Fixed URL routing for NavLink components

--- frontend/src/pages/scheduling/manage-spa.jsx
+++ frontend/src/pages/scheduling/manage-spa.jsx
@@ -11,18 +11,27 @@ export const ManageSpa = () => {
       <div>
         <div className="space-x-5 bg-white px-5 rounded-tl-2xl rounded-tr-2xl border-b border-b-gray-400">
           <NavLink
-            to={"/scheduling/manage/appointment"}
+            to={"/admin/scheduling/manage/appointment"}
             className={setActiveNav}
           >
             Appointment bulk edit
           </NavLink>
-          <NavLink to={"/scheduling/manage/open"} className={setActiveNav}>
+          <NavLink
+            to={"/admin/scheduling/manage/open"}
+            className={setActiveNav}
+          >
             Open time slots
           </NavLink>
-          <NavLink to={"/scheduling/manage/logs"} className={setActiveNav}>
+          <NavLink
+            to={"/admin/scheduling/manage/logs"}
+            className={setActiveNav}
+          >
             Activity logs
           </NavLink>
-          <NavLink to={"/scheduling/manage/process"} className={setActiveNav}>
+          <NavLink
+            to={"/admin/scheduling/manage/process"}
+            className={setActiveNav}
+          >
             Bulk process
           </NavLink>
         </div>
