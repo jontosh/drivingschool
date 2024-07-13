d08237cdbf2c1b4bdef46a49db5f43e3227c5a98
ali yuldashev <aliyuldashev880@gmail.com>
2024-07-13 14:20:21 +0500

    BACKEND: fixed usertype and changed student questions

--- backend/abstracts/urls.py
+++ backend/abstracts/urls.py
@@ -13,10 +13,10 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('', DocumentationView.as_view(),name="documentation"),
-    path('api/token/', TokenObtainPairView.as_view( ), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view( ), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view( ), name='token_verify'),
-    # path(f'{Router[ "authentication" ]["base"]}{Router[ "authentication" ][ "children" ][ "obtain_auth_token" ]}', obtain_auth_token,name='obtain_auth_token'),
-    # path(f'{Router[ "authentication" ]["base"]}{Router[ "authentication" ][ "children" ][ "login" ]}',custom_login, name='login'),
-    # path(f'{Router[ "authentication" ]["base"]}{Router[ "authentication" ][ "children" ][ "logout" ]}', auth_views.LogoutView.as_view(),name='logout'),
+    path(f'{Router[ "authentication" ]["base"]}{Router[ "authentication" ][ "children" ][ "token_obtain_pair" ]}', TokenObtainPairView.as_view( ), name='token_obtain_pair'),
+    path(f'{Router[ "authentication" ]["base"]}{Router[ "authentication" ][ "children" ][ "token_refresh" ]}', TokenRefreshView.as_view( ), name='token_refresh'),
+    path(f'{Router[ "authentication" ]["base"]}{Router[ "authentication" ][ "children" ][ "token_verify" ]}', TokenVerifyView.as_view( ), name='token_verify'),
+    # path(, obtain_auth_token,name='obtain_auth_token'),
+    # path(,custom_login, name='login'),
+    # path(, auth_views.LogoutView.as_view(),name='logout'),
 ]
