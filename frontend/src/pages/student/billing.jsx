4bfecfc61ad77ba56046048cedf94e65b088a312
KhaetbekIT <hayotbek-web@mail.ru>
2024-10-09 02:34:51 +0500

    #fix #js Fixed component editor and refactored student billing/enrollment page

--- frontend/src/pages/student/billing.jsx
+++ frontend/src/pages/student/billing.jsx
@@ -1,226 +1,134 @@
 import ButtonComponent from "@/components/button/index.jsx";
-import { CustomCheckBox } from "@/components/form/index.jsx";
-import Modal from "@/components/modal/index.jsx";
 import TableComponent from "@/components/table/index.jsx";
-import Title, { Paragraph } from "@/components/title/index.jsx";
+import Title from "@/components/title/index.jsx";
 import ColorsContext from "@/context/colors.jsx";
 import {
   StudentAccountEnrollmentModule,
   StudentAccountModule,
 } from "@/modules/student-account.jsx";
-import { ResultModalButton } from "@/pages/student/items/modal.jsx";
 import { useRequestGetQuery, useRequestIdQuery } from "@/redux/query/index.jsx";
-import MDEditor from "@uiw/react-md-editor";
-import classNames from "classnames";
-import { Fragment, useContext, useEffect, useReducer, useState } from "react";
-import { Form, Modal as ModalComponent } from "antd";
-import { useParams } from "react-router-dom";
-import rehypeSanitize from "rehype-sanitize";
-import BillingStyle from "./student-account.module.scss";
+import { Fragment, useContext, useEffect, useState } from "react";
+import { Button, Checkbox, Form, Input, Modal } from "antd";
 import { useURLSearchParams } from "@/hooks/useURLSearchParams.jsx";
-
-const reduce = (state, action) => {
-  const commonModalProps = {
-    open: action.open,
-    onOk: action.onEvent,
-    onCancel: action.onEvent,
-    title: action.title,
-    centered: true,
-    footer: null,
+import { Editor } from "@/components/editor";
+const onEmail = (Student, form) => {
+  const onFinish = async (values) => {
+    console.log(values);
   };
 
-  switch (action.type) {
-    case "print":
-      return {
-        ...state,
-        modal: (
-          <ModalComponent {...commonModalProps}>
-            <Form form={action.form} onFinish={action.onFinish}>
-              {["billing", "enrollment"].map((name) => (
-                <Form.Item key={name} name={name} valuePropName="checked">
-                  <CustomCheckBox>
-                    <Paragraph>
-                      {name === "billing" ? "Billing" : "Enrollment"}
-                    </Paragraph>
-                  </CustomCheckBox>
-                </Form.Item>
-              ))}
-              <Form.Item>
-                <CustomCheckBox>
-                  <Paragraph>
-                    3/9/2024 Teens 8hr in car instruction $649.99
-                  </Paragraph>
-                </CustomCheckBox>
-              </Form.Item>
-              <div className="text-center space-x-5">
-                <ButtonComponent
-                  defaultBg="#24C18F"
-                  defaultHoverBg="#24C18F"
-                  paddingInline={43}
-                  borderRadius={5}
-                  type="submit"
-                  controlHeight={50}
-                >
-                  Print
-                </ButtonComponent>
-                <ButtonComponent
-                  paddingInline={43}
-                  borderRadius={5}
-                  onClick={action.onReset}
-                  controlHeight={50}
-                  defaultBorderColor="#5459EA"
-                  defaultHoverBorderColor="#5459EA"
-                  defaultColor="#5459EA"
-                  defaultHoverColor="#5459EA"
-                >
-                  Cancel
-                </ButtonComponent>
-              </div>
-            </Form>
-          </ModalComponent>
-        ),
-      };
+  Modal.info({
+    title: "Email enrollment and/or Billing",
+    width: 1020,
+    content: (
+      <Form form={form} onFinish={onFinish}>
+        <Form.Item name={"email"} className={"mb-2"}>
+          <Checkbox>
+            <b className={"font-semibold"}>Student Email: {Student?.email}</b>
+          </Checkbox>
+        </Form.Item>
 
-    case "email":
-      return {
-        ...state,
-        modal: (
-          <ModalComponent {...commonModalProps}>
-            <Form
-              layout="vertical"
-              form={action.form}
-              onFinish={action.onFinish}
-            >
-              {["email", "parent_email", "parent_2_email"].map((name) => (
-                <Form.Item key={name} name={name} valuePropName="checked">
-                  <CustomCheckBox>
-                    <Paragraph>
-                      {name
-                        .replace("_", " ")
-                        .replace(/\b\w/g, (l) => l.toUpperCase())}
-                      : {action.form.getFieldValue(name)}
-                    </Paragraph>
-                  </CustomCheckBox>
-                </Form.Item>
-              ))}
-              {["enrollment", "teens"].map((name) => (
-                <Form.Item key={name} name={name} valuePropName="checked">
-                  <CustomCheckBox>
-                    <Paragraph>
-                      {name === "enrollment"
-                        ? "Enrollment"
-                        : "3/9/2024 Teens 8hr in car instruction $649.99"}
-                    </Paragraph>
-                  </CustomCheckBox>
-                </Form.Item>
-              ))}
-              <Form.Item name="" label="Enrollment And Billing Receipt">
-                <MDEditor
-                  placeholder="Text"
-                  previewOptions={{ rehypePlugins: [[rehypeSanitize]] }}
-                />
-              </Form.Item>
-              <div className="text-center space-x-5">
-                <ButtonComponent
-                  defaultBg="#24C18F"
-                  defaultHoverBg="#24C18F"
-                  paddingInline={43}
-                  borderRadius={5}
-                  type="submit"
-                  controlHeight={50}
-                >
-                  Print
-                </ButtonComponent>
-                <ButtonComponent
-                  paddingInline={43}
-                  borderRadius={5}
-                  onClick={action.onReset}
-                  controlHeight={50}
-                  defaultBorderColor="#5459EA"
-                  defaultHoverBorderColor="#5459EA"
-                  defaultColor="#5459EA"
-                  defaultHoverColor="#5459EA"
-                >
-                  Cancel
-                </ButtonComponent>
-              </div>
-            </Form>
-          </ModalComponent>
-        ),
-      };
+        <Form.Item name={"parent_email"} className={"mb-2"}>
+          <Checkbox>
+            <b className={"font-semibold"}>
+              Parent Email: {Student?.parent_email ?? "Not found"}
+            </b>
+          </Checkbox>
+        </Form.Item>
 
-    default:
-      console.error(`Unknown action: ${action.type}`);
-      return state;
-  }
+        <Form.Item name={"parent_2_email"} className={"mb-2"}>
+          <Checkbox>
+            <b className={"font-semibold"}>
+              Parent Email 2: {Student?.parent_2_email ?? "Not found"}
+            </b>
+          </Checkbox>
+        </Form.Item>
+
+        <Form.Item name={"enrollment"} className={"mb-2"}>
+          <Checkbox>
+            <b className={"font-semibold"}>Enrollment</b>
+          </Checkbox>
+        </Form.Item>
+
+        <Form.Item name={"start_date"} className={"mb-2"}>
+          <Checkbox>
+            <b className={"font-semibold"}>
+              3/9/2024 Teens 8hr in car instruction $649.99
+            </b>
+          </Checkbox>
+        </Form.Item>
+
+        <Form.Item
+          name={"subject"}
+          rules={[
+            {
+              required: true,
+              message: "Subject is required!",
+            },
+          ]}
+        >
+          <Input className={"h-[50px]"} placeholder={"Subject"} />
+        </Form.Item>
+
+        <Form.Item name={"body"}>
+          <Editor />
+        </Form.Item>
+
+        <Button type={"primary"} htmlType={"submit"}>
+          Send
+        </Button>
+      </Form>
+    ),
+  });
+};
+
+const onPrint = (form) => {
+  const onFinish = async (values) => {
+    console.log(values);
+  };
+
+  Modal.info({
+    title: "Print Enrollments and Billing Information",
+    content: (
+      <Form form={form} onFinish={onFinish}>
+        <Form.Item name={"bill"} className={"mb-2"}>
+          <Checkbox>
+            <b className={"font-semibold"}>Billing</b>
+          </Checkbox>
+        </Form.Item>
+
+        <Form.Item name={"enrollment"} className={"mb-2"}>
+          <Checkbox>
+            <b className={"font-semibold"}>Enrollment</b>
+          </Checkbox>
+        </Form.Item>
+
+        <Form.Item name={"start_date"} className={"mb-2"}>
+          <Checkbox>
+            <b className={"font-semibold"}>
+              3/9/2024 Teens 8hr in car instruction $649.99
+            </b>
+          </Checkbox>
+        </Form.Item>
+
+        <Button type={"primary"} htmlType={"submit"}>
+          Print
+        </Button>
+      </Form>
+    ),
+  });
 };
 
 const Enrollment = () => {
   const studentId = useURLSearchParams("studentId");
-  const { data: Packages } = useRequestGetQuery({
-    path: "/account_management/services/service/",
-  });
-  const { data: Instructor } = useRequestGetQuery({
-    path: "/student_account/instructor/",
-  });
-  const { data: StudentData } = useRequestIdQuery({
+  const { data: Student } = useRequestIdQuery({
     path: "/student_account/student",
     id: studentId,
   });
-
   const { colorsObject } = useContext(ColorsContext);
-  const [isOpen, setIsOpen] = useState(false);
   const [form] = Form.useForm();
-  const [state, dispatch] = useReducer(reduce, { modal: null, form });
+
   const { data, columns } = StudentAccountEnrollmentModule();
-  const [enrollmentData, setEnrollmentData] = useState([]);
-  const [price, setPrice] = useState(0);
-
-  useEffect(() => {
-    if (data && Packages && Instructor) {
-      const enrollment = data
-        .filter((item) => item.student === studentId)
-        .map((item) => ({
-          ...item,
-          package: Packages.find((pkg) => pkg.id === item.package)?.name,
-          instructorName: Instructor.find((inst) => inst.id === item.by)?.name,
-        }));
-      const totalPrice = enrollment.reduce((sum, item) => sum + item.price, 0);
-      setEnrollmentData(enrollment);
-      setPrice(totalPrice);
-    }
-  }, [data, Packages, Instructor, studentId]);
-
-  const handlePrint = () => {
-    dispatch({
-      type: "print",
-      title: "Print Enrollments and Billing Information",
-      onEvent: () => setIsOpen((prev) => !prev),
-      onFinish: (values) => console.log(values),
-      onReset: () => {
-        form.resetFields();
-        setIsOpen(false);
-      },
-      form,
-      open: !isOpen,
-    });
-  };
-
-  const handleEmail = () => {
-    form.setFieldsValue(StudentData);
-    dispatch({
-      type: "email",
-      title: "Print Enrollments and Billing Information",
-      onEvent: () => setIsOpen((prev) => !prev),
-      onFinish: (values) => console.log(values),
-      onReset: () => {
-        form.resetFields();
-        setIsOpen(false);
-      },
-      form,
-      open: !isOpen,
-    });
-  };
+  const totalPrice = data?.reduce((cur, acc) => cur + acc?.price, 0);
 
   return (
     <Fragment>
@@ -228,7 +136,7 @@ const Enrollment = () => {
         <div className="-mx-10 px-3 md:px-10 border-b pb-5 border-b-gray-400 flex items-center flex-col md:flex-row max-md:space-y-2.5 justify-between">
           <Title level={2} fontSize="text-xl space-x-2">
             <span className="text-indigo-700">Enrollment</span>
-            <span className="text-[#24C18F]">${price}</span>
+            <span className="text-[#24C18F]">${totalPrice}</span>
           </Title>
           <div className="grid min-[500px]:grid-cols-3 gap-2 max-md:w-full">
             <ButtonComponent
@@ -250,7 +158,7 @@ const Enrollment = () => {
               paddingInline={29}
               fontSize={14}
               borderRadius={5}
-              onClick={handleEmail}
+              onClick={() => onEmail(Student, form)}
             >
               Email
             </ButtonComponent>
@@ -262,16 +170,15 @@ const Enrollment = () => {
               paddingInline={29}
               fontSize={14}
               borderRadius={5}
-              onClick={handlePrint}
+              onClick={() => onPrint(form)}
             >
               Print
             </ButtonComponent>
           </div>
         </div>
         <div className="pt-5 -mx-10">
-          <TableComponent columns={columns} data={enrollmentData} />
+          <TableComponent columns={columns} data={data} rowKey="id" />
         </div>
-        {state?.modal}
       </div>
     </Fragment>
   );
@@ -282,17 +189,15 @@ const Billings = () => {
   const { data: Instructor } = useRequestGetQuery({
     path: "/student_account/instructor/",
   });
-  const { data: StudentData } = useRequestIdQuery({
+  const { data: Student } = useRequestIdQuery({
     path: "/student_account/student",
     id: studentId,
   });
   const { colorsObject } = useContext(ColorsContext);
   const { data, columns } = StudentAccountModule();
-  const [isOpen, setIsOpen] = useState(false);
   const [billingData, setBillingData] = useState([]);
   const [price, setPrice] = useState(0);
   const [form] = Form.useForm();
-  const [state, dispatch] = useReducer(reduce, { modal: null, form });
 
   useEffect(() => {
     if (data) {
@@ -302,22 +207,6 @@ const Billings = () => {
       setPrice(totalPrice);
     }
   }, [data, studentId]);
-
-  const handleAction = (type) => {
-    form.setFieldsValue(StudentData);
-    dispatch({
-      type,
-      title: "Print Enrollments and Billing Information",
-      onEvent: () => setIsOpen((prev) => !prev),
-      onFinish: (values) => console.log(values),
-      onReset: () => {
-        form.resetFields();
-        setIsOpen(false);
-      },
-      form,
-      open: !isOpen,
-    });
-  };
 
   return (
     <Fragment>
@@ -347,7 +236,7 @@ const Billings = () => {
               paddingInline={29}
               fontSize={14}
               borderRadius={5}
-              onClick={() => handleAction("email")}
+              onClick={() => onEmail(Student, form)}
             >
               Email
             </ButtonComponent>
@@ -359,7 +248,7 @@ const Billings = () => {
               paddingInline={29}
               fontSize={14}
               borderRadius={5}
-              onClick={() => handleAction("print")}
+              onClick={() => onPrint(form)}
             >
               Print
             </ButtonComponent>
@@ -369,7 +258,6 @@ const Billings = () => {
           <TableComponent columns={columns} data={billingData} />
         </div>
       </div>
-      {state?.modal}
     </Fragment>
   );
 };
